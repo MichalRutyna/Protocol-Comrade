4ba99cb498254338fb14e643cb15638010dac553
Michal <mrutyna@interia.pl>
2025-01-09 23:04:54 +0100

    Added enemy pathfinding and collision with player

--- src/game/templates/entities/BaseClassEnemy.gd
+++ src/game/templates/entities/BaseClassEnemy.gd
@@ -12,7 +12,7 @@ var velocity = Vector2(Vector2.ZERO)
 func _ready():
 	pass
 
-
+#region Movement
 func _physics_process(delta):
 	if velocity.x > 0:
 		orientation = 1
@@ -26,14 +26,42 @@ func _physics_process(delta):
 		velocity = velocity.normalized() * (speed + temp_speed)
 		temp_speed = 0
 	
-	position += velocity * delta
+	#position += velocity * delta
+	var closest_player = _find_closest_player()
+	position = position.move_toward(closest_player.position, (speed + temp_speed) * delta)
 	
+func _find_closest_player() -> Node :
+	"""
+		Returns Node closest_player
+	"""
+	var players = get_tree().get_nodes_in_group("Players")
+	if players.size() == 0:
+		print("Enemy pathfinding found 0 players in Players group")
+		return null
+	var closest_player_index = 0
+	var closest_player_distance = INF
+	for index in range(players.size()):
+		var distance = position.distance_squared_to(players[index].position)
+		if distance < closest_player_distance:
+			closest_player_index = index
+			closest_player_distance = distance
+	return players[closest_player_index]
+	
+
+#endregion
+
+#region Collision
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
-
-func _on_body_entered(body):
-	hide()
-	$CollisionShape2D.set_deferred("disabled", true)
-	SignalBus.enemyDied.emit()
+func _on_body_entered(body: Node2D):
+	if body.is_in_group("Players"):
+		print("Player hit")
+	else:
+		hide()
+		$CollisionShape2D.set_deferred("disabled", true)
+		SignalBus.enemyDied.emit()
+	
+	
+#endregion
